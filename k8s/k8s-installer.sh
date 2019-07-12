34f1fa954f9859085d171db5299ce5e77698fe20
jp-taku2 <taku.jp0526@gmail.com>
2019-07-12 18:01:19 +0900

    refact

--- k8s/k8s-installer.sh
+++ k8s/k8s-installer.sh
@@ -16,9 +16,9 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo swapoff -a
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-mkdir -p /home/vagrant/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-sudo chown $(id -u):$(id -g) /home/vagrant/.kube/config
+mkdir -p /root/.kube
+sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
+sudo chown $(id -u):$(id -g) /root/.kube/config
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 sudo kubectl taint nodes master node-role.kubernetes.io/master:NoSchedule-
